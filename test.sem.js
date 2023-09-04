b6b77652f2a9f2a934cd76ebc7384a9c8e13425d
Paul Tarvydas <guitarvydas@gmail.com>
2023-09-04 10:13:50 -0400

    simple test with iteration

--- test.sem.js
+++ test.sem.js
@@ -1,13 +1,11 @@
 {
     main : function (_k, _nl) {
-	var s = _k.walk ();
-	console.log ("in test.sem.js/main...");
-	console.log (s);
+	var s = _k.walk ().join ('');
 	return s;
     },
 
     _terminal: function () { return this.sourceString; },
-    _iter: function (...children) { return children.map(c => c._rwr ()); },
+    _iter: function (...children) { return children.map(c => c.walk ()); },
     spaces: function (x) { return this.sourceString; },
     space: function (x) { return this.sourceString; }
 }
