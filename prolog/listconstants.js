e4f3b59ab2fc9089a0060167518e2145717ad376
Paul Tarvydas <guitarvydas@gmail.com>
2023-09-06 08:41:56 -0400

    begin debugging listconstants

--- prolog/listconstants.js
+++ prolog/listconstants.js
@@ -1,5 +1,10 @@
+_ = {
+    toSpaceDelimitedList: function (a) { return a.join(' '); },
+    toPackedString: function (a) { return a.join(''); }
+},
+
 {
-    Program: function(tree) {return toSpaceDelimitedList(tree.rwr())},
+    Program: function(tree) {return _.toSpaceDelimitedList(tree.rwr())},
     Form: function(item) {return item.rwr()},
     SList: function(lis) {return lis.rwr();},
     QuotedSexp: function(_lp,_q,form,_rp) {return form.rwr()},
@@ -7,15 +12,15 @@
     CommaSexp: function(_, form) {throw "can't happen";},
     DottedList: function(_lp, items, _dot, lastItem, _rp) { throw "this solution does not support dotted list (except when quoted)"},
     NullTerminatedList: function(_lp, items, _rp) {
-	return "(" + toSpaceDelimitedList(items.rwr()) + ")"},
+	return "(" + _.toSpaceDelimitedList(items.rwr()) + ")"},
     ListItem: function(item) {return item.rwr()},
     Atom: function(a) {return a.rwr()},
     Syntactic_Atom: function(a) { return a.rwr();},
 
     QuotedSList: function(slist) { return slist.rwr(); },
-    QuotedNullTerminatedList: function(_lp,qitem,_rp) { return "(@newList@ " + toSpaceDelimitedList(qitem.rwr()) + ")"; },
+    QuotedNullTerminatedList: function(_lp,qitem,_rp) { return "(@newList@ " + _.toSpaceDelimitedList(qitem.rwr()) + ")"; },
     QuotedDottedList: function(_lp, items, _dot, lastItem, _rp) {
-	return "(@newDottedList@ " + toSpaceDelimitedList(items.rwr()) + " " + lastItem.rwr() + ")"},
+	return "(@newDottedList@ " + _.toSpaceDelimitedList(items.rwr()) + " " + lastItem.rwr() + ")"},
     QListItem: function(item) { return item.rwr(); },
     QAtom: function(a) { return a.rwr(); },
     QAtomicNonSymbol: function(a) { return a.rwr(); },
@@ -25,10 +30,10 @@
 
     lexical_atom: function(a) {return a.rwr(); },
     lexical_boolean: function(b) {return this.sourceString},
-    lexical_integer: function(ns) {return toPackedString(ns.rwr());},
+    lexical_integer: function(ns) {return _.toPackedString(ns.rwr());},
     lexical_numchar: function(c) {return c.rwr()},
-    lexical_string: function(_q1, chars, _q2) {return "\"" + toPackedString(chars.rwr()) + "\""},
-    lexical_symbol: function(c, cs) {return c.rwr() + toPackedString(cs.rwr());},
+    lexical_string: function(_q1, chars, _q2) {return "\"" + _.toPackedString(chars.rwr()) + "\""},
+    lexical_symbol: function(c, cs) {return c.rwr() + _.toPackedString(cs.rwr());},
     lexical_letchar: function(c) {return c.rwr()},
     lexical_lc: function(c) {return c.rwr()},
     lexical_uc: function(c) {return c.rwr()},
