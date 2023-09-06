a3f70390526f012d2e1a6c0e9a6cfedde2eb6e03
Paul Tarvydas <guitarvydas@gmail.com>
2023-09-06 07:36:35 -0400

    relabel to identity

--- prolog/unbq.js
+++ prolog/unbq.js
@@ -34,31 +34,31 @@ Scm {
 `;
 
 
-var unity_sem =
+var identity_sem =
     {
-	Program: function(tree) {return toSpaceDelimitedList(tree.unity())},
-	Form: function(item) {return item.unity()},
-	QuotedSexp: function(_, form) {return "'" + form.unity()},
-	BackQuotedSexp: function(_, form) {return "`" + form.unity()},
-	CommaSexp: function(_, form) {return "," + form.unity()},
-	SList: function(lis) {return lis.unity()},
+	Program: function(tree) {return toSpaceDelimitedList(tree.identity())},
+	Form: function(item) {return item.identity()},
+	QuotedSexp: function(_, form) {return "'" + form.identity()},
+	BackQuotedSexp: function(_, form) {return "`" + form.identity()},
+	CommaSexp: function(_, form) {return "," + form.identity()},
+	SList: function(lis) {return lis.identity()},
 	DottedList: function(_lp, items, _dot, lastItem, _rp) {
-	    return "(" + toSpaceDelimitedList(items.unity()) + " . " + lastItem.unity() + ")"},
+	    return "(" + toSpaceDelimitedList(items.identity()) + " . " + lastItem.identity() + ")"},
 	NullTerminatedList: function(_lp, items, _rp) {
-	    return "(" + toSpaceDelimitedList(items.unity()) + ")"},
-	ListItem: function(item) {return item.unity()},
-	Atom: function(a) {return a.unity()},
-	lexical_integer: function(ns) {return toPackedString(ns.unity());},
-	lexical_symbol: function(c, cs) {return c.unity() + toPackedString(cs.unity());},
-	lexical_string: function(_q1, chars, _q2) {return "\"" + toPackedString(chars.unity()) + "\""},
-	lexical_letchar: function(c) {return c.unity()},
-	lexical_numchar: function(c) {return c.unity()},
-	lexical_lc: function(c) {return c.unity()},
-	lexical_uc: function(c) {return c.unity()},
+	    return "(" + toSpaceDelimitedList(items.identity()) + ")"},
+	ListItem: function(item) {return item.identity()},
+	Atom: function(a) {return a.identity()},
+	lexical_integer: function(ns) {return toPackedString(ns.identity());},
+	lexical_symbol: function(c, cs) {return c.identity() + toPackedString(cs.identity());},
+	lexical_string: function(_q1, chars, _q2) {return "\"" + toPackedString(chars.identity()) + "\""},
+	lexical_letchar: function(c) {return c.identity()},
+	lexical_numchar: function(c) {return c.identity()},
+	lexical_lc: function(c) {return c.identity()},
+	lexical_uc: function(c) {return c.identity()},
 
 	lexical_boolean: function(b) {return this.sourceString},
 	_terminal: function() { return this.sourceString; },
-	_iter: function (...children) { return children.map(c => c.unity ()); }
+	_iter: function (...children) { return children.map(c => c.identity ()); }
     };
 
 
@@ -96,7 +96,7 @@ var inbq_sem =
 	Form: function(item) {return item.inbackquote()},
 	QuotedSexp: function(_, form) {return "(quote " + form.inbackquote() + ")"},
 	BackQuotedSexp: function(_, form) {throw "can\'t happen - left as exercise to the reader";},
-	CommaSexp: function(_, form) {return form.unity()}, // use unbackquote to get eval'ed value
+	CommaSexp: function(_, form) {return form.identity()}, // use unbackquote to get eval'ed value
 	SList: function(lis) {return lis.inbackquote()},
 	DottedList: function(_lp, items, _dot, lastItem, _rp) {
 	    return "(list " + toSpaceDelimitedList(items.inbackquote()) + " . " + lastItem.inbackquote() + ")"},
@@ -121,7 +121,7 @@ var inbq_sem =
 function unbq (s) {
     let grammar = ohm.grammar (grammarText);
     let semantics = grammar.createSemantics();
-    semantics.addOperation('unity', unity_sem);
+    semantics.addOperation('identity', identity_sem);
     semantics.addOperation('unbackquote', unbq_sem);
     semantics.addOperation('inbackquote', inbq_sem);
     let parsed = grammar.match(s);
