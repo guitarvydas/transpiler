d07061683dd3b918b39bbb397783daa0bcdc7460
Paul Tarvydas <guitarvydas@gmail.com>
2023-09-07 22:25:12 -0400

    virtualComma

---
+++ prolog/virtualcomma.js
@@ -0,0 +1,16 @@
+// vcomma = "○￮￮₊˚
+
+vcomma = "₊",
+
+{
+    input : function (cs) { return cs.rwr ().join(''); },
+    char_space: function (c) { return c.rwr () },
+    char_separator: function (c) { return c.rwr (); },
+    char_needsVcomma: function (c, lookahead) { return c.rwr () + vcomma; },
+    char_doesNotNeedVcomma: function (c) { return c.rwr () },
+    separator: function (c) { return c.rwr () },
+    semiColonComment: function (semicolon, cs, nl) { return semicolon.rwr () + cs.rwr ().join ('') + nl.rwr (); },
+
+    _terminal: function() { return this.sourceString; },
+    _iter: function (...children) { return children.map(c => c.rwr ()); }
+}
