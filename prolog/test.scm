178571def87dc977249e07be64cd1602c1de3cbc
Paul Tarvydas <guitarvydas@gmail.com>
2023-09-16 07:12:50 -0400

    obsvn: mutate macro skips leading space

--- prolog/test.scm
+++ prolog/test.scm
@@ -1,22 +1,28 @@
 (define (call-foreign expr bindings)
-  (let ((func (cadr expr))
-	(args (cddr expr)))
+    (cond 
+	  ((b g)
+	   (f 4))
+	  ))
 
-    (cond ((string=? "unity" func)
-	   (car args))
+;; (define (call-foreign expr bindings)
+;;   (let ((func (cadr expr))
+;; 	(args (cddr expr)))
 
-	  ((string=? "add" func)
-	   (let ((resolved-args (resolveArgs args bindings)))
-	     (+ (car resolved-args) (cadr resolved-args))))
+;;     (cond ((string=? "unity" func)
+;; 	   (car args))
 
-	  ((string=? "display" func)
-	   (let ((a (value (car args) bindings)))
-	     (display a)))
+;; 	  ((string=? "add" func)
+;; 	   (let ((resolved-args (resolveArgs args bindings)))
+;; 	     (+ (car resolved-args) (cadr resolved-args))))
+
+;; 	  ((string=? "display" func)
+;; 	   (let ((a (value (car args) bindings)))
+;; 	     (display a)))
 	  
-	  ((string=? "newline" func)
-	   (newline))
+;; 	  ((string=? "newline" func)
+;; 	   (newline))
 	  
-	  (else (error "call-foreign called with unknown operator" func)))))
+;; 	  (else (error "call-foreign called with unknown operator" func)))))
 
 ;; (define (junk2)
 ;;   (if x
